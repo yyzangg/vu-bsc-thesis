b52d80b4bf0e492067b936163cb55709da17cda8
yyzangg <tmedcz@outlook.com>
2024-06-24 00:43:48 +0200

    Add `pred_albert_base`

---
+++ workload-prediction/pred_albert_base/pred_albert_base.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --time=00:15:00                    # FixMe
+#SBATCH -N 1
+#SBATCH -C gpunode                         # FixMe
+#SBATCH --gres=gpu:1                       # Number of GPUs needed
+
+#SBATCH --output=pred_out_albert_base.log  # Output file
+#SBATCH --error=pred_err_albert_base.log   # Error file
+
+. /etc/bashrc
+. /etc/profile.d/modules.sh
+
+# Load pyenv
+export PYENV_ROOT="/var/scratch/yzg244/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+# Set the desired Python version
+pyenv global 3.9.13
+
+cd /home/yzg244/pred_albert_base
+
+# Run the Python script
+echo "Running pred_albert_base.py..."
+python pred_albert_base.py
+
+echo "Script execution completed."
